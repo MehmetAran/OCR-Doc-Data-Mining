a8bbe96f08c6aba6919dda74948bc00a4d5d73d2
160202030 <160202030@kocaeli.edu.tr>
2020-05-08 17:25:51 +0300

    unnecessary files is deleted

--- src/python/docOkut.py
+++ src/python/docOkut.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'docOkut.ui'
-#
-# Created by: PyQt5 UI code generator 5.14.1
-#
-# WARNING! All changes made in this file will be lost!
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
